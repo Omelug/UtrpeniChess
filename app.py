20324c5b31c1eb7285cb0d1572a6c29fb03a2ff1
witherk1ng <troll1239@seznam.cz>
2024-07-05 19:50:45 +0200

    JavaScript - tuk tuk

--- app.py
+++ app.py
@@ -6,22 +6,31 @@ app = Flask(__name__)
 
 games={}
 @app.route('/')
-def index():
+def home():
     return redirect(url_for('index'))
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/create_game', method='POST')
+@app.route('/create_game', methods=['POST'])
 def create_game():
+
     game = Game()
-    response = make_response(render_template('index.html'))
-    response.set_cookie('game_code', game.code)
     player = Player(starter=True)
-    response.set_cookie('player_id', player.player_uuid)
     game.add_player(player)
+
+    response = make_response(redirect(url_for('board')))
+    response.set_cookie('game_code', game.code)
+    response.set_cookie('player_uuid', player.player_uuid)
+
     return response
 
+@app.route('/board')
+def board():
+    game_code = request.cookies.get('game_code')
+    player_uuid = request.cookies.get('player_uuid')
+    response = make_response(render_template('board.html', player_uuid=player_uuid, game_code=game_code))
+    return response
 
 if __name__ == '__main__':
     app.run()
