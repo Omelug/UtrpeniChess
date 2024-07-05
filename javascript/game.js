d746f470e95bd316085a928eb544afdfc7f96edf
witherk1ng <troll1239@seznam.cz>
2024-07-05 20:36:25 +0200

    fiix v1

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,4 @@
-document.getElementById('createGameButton').addEventListener('click', function() {
+document.getElementById('createGame').addEventListener('click', function() {
     fetch('/create_game', {
         method: 'POST',
         headers: {
