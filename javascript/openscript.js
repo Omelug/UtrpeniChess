1d466437f2bc3050d18518ca83a31ac0ff657d94
witherk1ng <troll1239@seznam.cz>
2024-07-07 00:24:22 +0200

    Huge CSS update -Wither 0:23 ráno

--- javascript/openscript.js
+++ javascript/openscript.js
@@ -3,4 +3,5 @@ const button = document.getElementById('gamemenu');
 button.addEventListener('click', function() {
     openn.classList.add("open-menu");
     openn.classList.add("panel-menu");
+
 });
